a0b41afa57be7b756a1fc5b3abc3e27bfddd4725
Kwiggler <kwiggin7@jhmi.edu>
2024-02-23 09:16:03 -0500

    Employee done

--- FAP.py
+++ FAP.py
@@ -4,6 +4,7 @@ import csv
 
 
 file_path = "settings.txt"
+#"(1291, 105)" = (1291, 105)
 pyautogui.FAILSAFE = True  # Enable failsafe by moving the mouse to the upper left corner
     
 #Functions 
@@ -19,26 +20,41 @@ def move_and_click(x, y, delay=0.7):
     pyautogui.click()
     time.sleep(delay)
 
+
+def tabto(count):
+    move_and_click(1291, 105)
+    for _ in range(count):
+        pyautogui.press('tab')
+        time.sleep(0.1)
+        
+        
 # Function to press tab the required number of times and then either up+tab or down based on the setting
 def press_tab_and_decide(setting, tabs_count):
     # Press tab the required number of times
-    for _ in range(tabs_count):
-        pyautogui.press('tab')
-        time.sleep(0.9)  # Adding a small delay to ensure the UI can catch up
-
+    tabto(tabs_count)
+    
     # Decide whether to press 'up' and 'tab' or just 'down'
     if setting.lower() == 'allowed':
         pyautogui.hotkey('up')
         time.sleep(1.2)
-        pyautogui.hotkey('tab')
     else:
         pyautogui.press('down')
     time.sleep(0.7)
 
 
+
+def tab_and_open(count):
+    tabto(count)
+    pyautogui.press('enter')
+    time.sleep(1)
+        
+
+    
+
 # Read the settings from the file
 with open(file_path, 'r') as file:
     csv_reader = csv.DictReader(file)
+    
     for row in csv_reader:
         # Assuming the first row is the header with setting names
         for setting, value in row.items():
@@ -46,16 +62,16 @@ with open(file_path, 'r') as file:
             # Perform the action based on the setting value
             if setting == "Everyone":
                 #Everyone (Expand) Mouse Click (-1664,69)
-                move_and_click(-1664, 69)
+                
+               #time.sleep(1)
+               ##Everyone (Mouse Click in front (-1631,66), Click on two tabs, and then click up + down (Disallow All))
+               #move_and_click(-1631, 66)
+               #time.sleep(1)
+               #pyautogui.press(['tab', 'tab'])
+               #time.sleep(2)
+               #pyautogui.hotkey('up', 'down')
                 time.sleep(1)
-                #Everyone (Mouse Click in front (-1631,66), Click on two tabs, and then click up + down (Disallow All))
-                move_and_click(-1631, 66)
-                time.sleep(1)
-                pyautogui.press(['tab', 'tab'])
-                time.sleep(2)
-                pyautogui.hotkey('up', 'down')
-                time.sleep(1)
-                pyautogui.press(['tab'])
+               # pyautogui.press(['tab'])
 
             elif  setting == "Open an online form":
                 press_tab_and_decide(value,9)
@@ -65,182 +81,174 @@ with open(file_path, 'r') as file:
                 
             elif  setting == "Employee":
                 #Employee (Expand) Mouse Click (-1664,69)
-                 move_and_click(1015, 765)
+                 tab_and_open(17)
                  time.sleep(1)
-                 #Employee (Mouse Click in front (-1631,66), Click on two tabs, and then click up + down (Disallow All))
-                 move_and_click(1050, 185)
-                 time.sleep(1)
-                 pyautogui.press(['tab', 'tab'])
-                 time.sleep(2)
+                 tabto(18)
                  pyautogui.hotkey('up', 'down')
                  time.sleep(1)
-                 pyautogui.press(['tab'])
-                 
+                 tabto(18)
+                 #pyautogui.press(['tab'])
+                 #
             elif  setting == "Time Stamp for Employees":
                 #Time Stamp for Employees (Expand) 
-                 move_and_click(1078,213)
-                 time.sleep(1)
-                 #Time Stamp for Employees
-                 move_and_click(1165,181)
-                 time.sleep(1)
-                 pyautogui.press(['tab', 'tab'])
+                 tab_and_open(20)
                  time.sleep(2)
 
             elif  setting == "Cancel meal deductions in Time Stamp":
-                press_tab_and_decide(value,2)
-                
+                press_tab_and_decide(value,23)
+                # end of Time Stamp for Employees. Closing group    
+                tab_and_open(20)
+                time.sleep(2)
+            
             elif  setting == "Calendar":
                 #Time Stamp for Employees (Expand) 
-                 move_and_click(1033, 304)
-                 time.sleep(1)
-                 #Time Stamp for Employees
-                 move_and_click(1079, 182)
-                 time.sleep(1)
-                 pyautogui.press(['tab', 'tab'])
+                 tab_and_open(23)
                  time.sleep(2)
                  
             elif  setting == "Calendar views for employees":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,26)
                 
             elif  setting == "Print Schedule":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,28)
                 
             elif  setting == "Employee Visibility Periods":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,30)
+                # end of Calendar. Closing group    
+                tab_and_open(23)
+                time.sleep(2)
                 
             elif  setting == "Timecard Editor for Employees (My Timecard)":
-                #Time Stamp for Employees (Expand) 
-                 move_and_click(1100, 303)
-                 time.sleep(1)
-                 #Time Stamp for Employees
-                 move_and_click(1265,175)
-                 time.sleep(1)
-                 pyautogui.press(['tab', 'tab'])
+                #Timecard Editor for Employees (My Timecard) (Expand) 
+                 tab_and_open(30)
                  time.sleep(2)
                  
             elif  setting == "Comments in My Timecard - Add":
-                press_tab_and_decide(value,9)
+                press_tab_and_decide(value,40)
                 
             elif  setting == "Comments in My Timecard - Delete":
-                press_tab_and_decide(value,1)
+                press_tab_and_decide(value,41)
                 
             elif  setting == "Notes for Comments in My Timecard":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,43)
                 
             elif  setting == "Hours Worked amount in My Timecard":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,45)
                 
                 
             elif  setting == "Pay Codes in My Timecard - Edit":
                 #Pay Codes in My TImecard (Expand)
-                move_and_click(1091, 354)
-                time.sleep(1)
-                #Time Stamp for Employees
-                move_and_click(1180,180)
-                time.sleep(1)
-                pyautogui.press(['tab', 'tab'])
+                tab_and_open(47)
                 time.sleep(2)
                 
-                press_tab_and_decide(value,2)
+                #Check pay code - edit field 
+                press_tab_and_decide(value,50)
                 
             elif  setting == "Pay Codes in My Timecard - View":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,52)
                 
             elif  setting == "Move Amounts in My Timecard":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,54)
+                # End of group. Pay Codes in My TImecard (Close)
+                tab_and_open(47)
+                time.sleep(2)
                 
             elif  setting == "Punch edits in My Timecard":
-                press_tab_and_decide(value,4)
+                press_tab_and_decide(value,50)
                 
             elif  setting == "Calculate totals in My Timecard":
-                press_tab_and_decide(value,5)
+                press_tab_and_decide(value,55)
                 
             elif  setting == "Totals breakdown in My Timecard":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,57)
                 
             elif  setting == "View schedules in My timecard":
-                press_tab_and_decide(value,4)
+                press_tab_and_decide(value,61)
                 
             elif  setting == "View the Audit Trail Tab in My Timecard":
-                press_tab_and_decide(value,12)
-                
-            elif  setting == "Activities in My Timecard":
-                #Pay Codes in My TImecard (Expand)
-                move_and_click(1067, 702)
-                time.sleep(1)
-                #Time Stamp for Employees
-                move_and_click(1157,179)
-                time.sleep(1)
-                pyautogui.press(['tab', 'tab'])
+                press_tab_and_decide(value,73)
+                #Timecard Editor for Employees (My Timecard) (close) 
+                tab_and_open(30)
                 time.sleep(2)
+                
+            #elif  setting == "Activities in My Timecard":
+            #    tab_and_open(33)
+            #    time.sleep(2)
             
             elif  setting == "Transfers - View transfers":
                 #Transfers (Expand)
-                move_and_click(1041, 390)
-                time.sleep(1)
-                #Time Stamp for Employees
-                move_and_click(1072,210)
-                time.sleep(1)
-                pyautogui.press(['tab', 'tab'])
+                tab_and_open(36)
                 time.sleep(2)
-                
-                press_tab_and_decide(value,2)  
+                press_tab_and_decide(value,39)  
                 
             elif  setting == "Perform cost center transfers":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,41)
                 
             elif  setting == "Perform labor category transfers":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,43)
                 
             elif  setting == "Perform work rule transfers":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,45)
                 
             elif  setting == "Perform job transfers":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,47)
+                #Transfers (close)
+                tab_and_open(36)
+                time.sleep(2)
                 
             elif  setting == "Access to Employee Home Page":
-                press_tab_and_decide(value,12)
+                press_tab_and_decide(value,49)
                 
             elif  setting == "Employee access to My Actions list":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,51)
             
             elif  setting == "Location Data":
-                #Location Data (Expand)
-                move_and_click(1049, 512)
-                time.sleep(1)
-                #Time Stamp for Employees
-                move_and_click(1105,361)
-                time.sleep(1)
-                pyautogui.press(['tab', 'tab'])
-                time.sleep(2) 
+                #Location Data (expand)
+                tab_and_open(53)
+                time.sleep(2)
                 
             elif  setting == "View location data":
-                press_tab_and_decide(value,4)
+                press_tab_and_decide(value,58)
+                #Location Data (close)
+                tab_and_open(53)
+                time.sleep(2)
                 
             elif  setting == "Enable location schedule":
-                press_tab_and_decide(value,12)
+                press_tab_and_decide(value,58)
                 
             elif  setting == "Access unposted schedule":
-                press_tab_and_decide(value,9)
+                press_tab_and_decide(value,67)
                 
             elif  setting == "My Requests":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,69)
                 
             elif  setting == "Comments & Notes in my request":
-                press_tab_and_decide(value,5)
+                press_tab_and_decide(value,74)
                 
             elif  setting == "Impersonation Access":
-                press_tab_and_decide(value,2)
+                press_tab_and_decide(value,76)
                 
             elif  setting == "Offline":
-                #Location Data (Expand)
-                move_and_click(1032, 788)
-                time.sleep(1)
-                #Time Stamp for Employees
-                move_and_click(1057,760)
-                time.sleep(1)
-                pyautogui.press(['tab', 'tab'])
-                time.sleep(2) 
+                #Offline (expand)
+                tab_and_open(78)
+                time.sleep(2)
                 
             elif  setting == "Mobile App Punch":
-                press_tab_and_decide(value,2) 
+                press_tab_and_decide(value,81) 
+                
+                
+                
+                
+            #### Dept Manager ####
+                
+            # if setting is Timecard Editor for Managers click and open()
+            
+            # if Timecard access
+                # tab and decide() but add conditions for 'All' and 'All but self'
+                # press up for all but self
+                # press a for all
+                
+            # if Allow negatives in Timecard Editor tab and decide
+            
+            # if Approval in Timecard Editor open and select
+            
+            # 
